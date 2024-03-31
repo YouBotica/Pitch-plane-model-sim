6a9f8c81e00c5747364fe2469af73d1b82e268b7
Andres <ahoyos@academia.usbbog.edu.co>
2024-03-30 21:57:32 -0400

    Calculated both spring configurations (2 Hz ride and Olley Ride Criteria)

--- simulation3.m
+++ simulation3.m
@@ -25,48 +25,53 @@ K1 = 0.7*K2;
 
 Kb = K1*l1 - K2*l2; 
 
-%% Linear pitch-plane model:
+pitch_plane_model(K1, K2, C1, C2, l1, l2, ms, Iy);
 
-A = [-Ka/ms, Kb/ms; 
-     Kb/Iy, -Kc/Iy];
 
-B = [K1/ms, K2/ms; -l1*K1/Iy, l2*K2/Iy];
-C = [1, 0];
-D = [0, 0];
+%% 2.)  Using Olley ride criteria determine appropriate front and rear corner stiffnesses.
 
-%%
+omega_des = (2*pi); % 1 Hz ride and heave desired frequencies
 
-% A = [
-%     0, 1, 0, 0;
-%     (-K1 - K2)/ms, 0, (K1*l1 - K2*l2)/ms, 0;
-%     0, 0, 0, 1;
-%     (K1*l1 - K2*l2)/Iy, 0, (-K1*l1^2 - K2*l2^2)/Iy, 0;
-%     ];
+% x_sp = -0.08*2*l1;
 
-A = [
-    0, 1, 0, 0;
-    (-K1 - K2)/ms, 0, (K1*l1 - K2*l2)/ms, 0;
-    0, 0, 0, 1;
-    (K1*l1 - K2*l2)/Iy, 0, (-K1*l1^2 - K2*l2^2)/Iy, 0;
-    ];
+Ka = ms*omega_des^2; %(ms / (1 - x_sp*sqrt(ms/Iy)))*omega_des^2;
+Kc = Iy*omega_des^2; %(Iy / (1 - x_sp*sqrt(ms/Iy)))*omega_des^2;
 
-B = [K1/ms, K2/ms; 0, 0; -l1*K1/Iy, l2*K2/Iy; 0, 0];
+K2 = Ka / 1.7;
+K1 = 0.7*K2;
 
-C = [1, 0, 0, 0; 0, 0, 1, 0];
-D = [0, 0; 0, 0];
+Kb = K1*l1 - K2*l2; 
 
-[magx,phasex,wx]=bode(ss(A,B(:,1),C(1,:),[0]),logspace(0,2));
- mgx(1:50)=magx;
- phx(1:50)=phasex;
- 
-[magtheta,phasetheta,wtheta]=bode(ss(A,B(:,1),C(2,:),[0]),logspace(0,2));
- mgth(1:50)=magtheta;
- phth(1:50)=phasetheta;
- 
-subplot(1,1,1),semilogx(wx,20*log10(mgx),wtheta,20*log10(mgth))
+pitch_plane_model(K1, K2, C1, C2, l1, l2, ms, Iy);
 
 %%
-%%pitch_plane_model = ss(A, B(:,1), C, [0]);
+
+function pitch_plane_model(K1, K2, C1, C2, l1, l2, ms, Iy)
+
+    A = [
+        0, 1, 0, 0;
+        (-K1 - K2)/ms, 0, (K1*l1 - K2*l2)/ms, 0;
+        0, 0, 0, 1;
+        (K1*l1 - K2*l2)/Iy, 0, (-K1*l1^2 - K2*l2^2)/Iy, 0;
+        ];
+    
+    B = [K1/ms, K2/ms; 0, 0; -l1*K1/Iy, l2*K2/Iy; 0, 0];
+    
+    C = [1, 0, 0, 0; 0, 0, 1, 0];
+    D = [0, 0; 0, 0];
+    
+    
+    [magx,phasex,wx]=bode(ss(A,B(:,1),C(1,:),[0]),logspace(0,2));
+     mgx(1:50)=magx;
+     phx(1:50)=phasex;
+     
+    [magtheta,phasetheta,wtheta]=bode(ss(A,B(:,1),C(2,:),[0]),logspace(0,2));
+     mgth(1:50)=magtheta;
+     phth(1:50)=phasetheta;
+     
+    subplot(1,1,1),semilogx(wx,20*log10(mgx),wtheta,20*log10(mgth))
+
+end
 
 
 
